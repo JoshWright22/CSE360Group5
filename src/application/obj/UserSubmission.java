bf56a6483e55b0dec83adc61c2d0fb4d12911bcd
syntacsdev <syntaxiomonline@gmail.com>
2025-10-12 16:16:52 -0700

    Added commentary to UserSubmission.java

--- src/application/obj/UserSubmission.java
+++ src/application/obj/UserSubmission.java
@@ -2,40 +2,45 @@ package application.obj;
 
 import java.time.LocalDateTime;
 
+/**
+ * A parent class that is extended by the Question, Answer, and Comment classes.
+ */
 public class UserSubmission {
 
-	protected int id;
-	protected String userName;
-	protected LocalDateTime creationDate;
+	protected final int id;
+	protected final String userName;
+	protected final LocalDateTime creationDate;
 	
 	public UserSubmission(int id, String userName, LocalDateTime creationDate) {
 		this.id = id;
 		this.userName = userName;
 		this.creationDate = creationDate;
 	}
-	
+
+	/**
+	 * Gets the ID of the UserSubmission.
+	 * 
+	 * @return	int
+	 */
 	public int getId() {
 		return this.id;
 	}
 	
+	/**
+	 * Gets the name of the user who made the UserSubmission.
+	 * 
+	 * @return	String
+	 */
 	public String getUserName() {
 		return this.userName;
 	}
 	
+	/**
+	 * Gets the creation date/time of the UserSubmission.
+	 * 
+	 * @return	LocalDateTime
+	 */
 	public LocalDateTime getCreationDate() {
 		return this.creationDate;
 	}
-	
-	public void setId(int id) {
-		this.id = id;
-	}
-	
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	
-	public void setCreationDate(LocalDateTime creationDate) {
-		this.creationDate = creationDate;
-	}
-	
 }
