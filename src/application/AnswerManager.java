08ee5ea4beb3bd1869556f132c7d15a8ff515e4b
syntacsdev <syntaxiomonline@gmail.com>
2025-11-07 14:26:42 -0700

    Organized imports and did some slight formatting

--- src/application/AnswerManager.java
+++ src/application/AnswerManager.java
@@ -5,46 +5,44 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import application.obj.Answer;
-import application.obj.Question;
 import databasePart1.DatabaseHelper;
 
 /**
- * Acts as a manager class for dealing with Answer objects. This use of data hiding and encapsulation helps ensure parity
- * between data stored in the local cache and data stored on the MySQL database.
+ * Acts as a manager class for dealing with Answer objects. This use of data
+ * hiding and encapsulation helps ensure parity between data stored in the local
+ * cache and data stored on the MySQL database.
  */
 public class AnswerManager {
 
 	private final DatabaseHelper database;
 	private final Set<Answer> answerSet = new HashSet<>();
-	
+
 	public AnswerManager(DatabaseHelper database) {
 		this.database = database;
 	}
-	
+
 	/**
 	 * Attempts to fetch all data from the Answers table.
 	 */
 	public void fetchAnswers() {
 		String query = "SELECT * FROM Answers";
 		try (ResultSet rs = this.database.getStatement().executeQuery(query)) {
-			
+
 			answerSet.clear();
-			
+
 			while (rs.next()) {
 				// Collect required information to construct Answer object
 				int id = rs.getInt("id");
 				String userName = rs.getString("userName");
 				LocalDateTime creationDate = LocalDateTime.parse(rs.getString("creationDate"));
 				String content = rs.getString("content");
-				//List<String> tags = Arrays.asList(rs.getString("tags").split(","));
-				
+				// List<String> tags = Arrays.asList(rs.getString("tags").split(","));
+
 				// Construct answer and add it into local cache
 				Answer a = new Answer(id, userName, creationDate, content, null);
 				this.answerSet.add(a);
@@ -54,16 +52,16 @@ public class AnswerManager {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Fetches a particular answer from the database given the answer's ID.
 	 * 
-	 * @param id	ID of answer whose data will be fetched
-	 * @return		Answer object constructed from database data
+	 * @param id ID of answer whose data will be fetched
+	 * @return Answer object constructed from database data
 	 */
 	public Answer fetchAnswer(int id) {
 		Answer result = null;
-		
+
 		String query = "SELECT * FROM Answers WHERE id = ?";
 		try (PreparedStatement stmt = this.database.getConnection().prepareStatement(query)) {
 			stmt.setInt(1, id);
@@ -73,27 +71,28 @@ public class AnswerManager {
 				String userName = rs.getString("userName");
 				LocalDateTime creationTime = LocalDateTime.parse(rs.getString("creationDate"));
 				String content = rs.getString("content");
-				//List<String> tags = Arrays.asList(rs.getString("tags").split(","));
-				
+				// List<String> tags = Arrays.asList(rs.getString("tags").split(","));
+
 				result = new Answer(i, userName, creationTime, content, null);
 			}
 		} catch (SQLException e) {
 			System.err.println("Failed to fetch answer from database.");
 			e.printStackTrace();
 		}
-		
+
 		return result;
 	}
-	
+
 	/**
-	 * Gets an unmodifiable reference to the set of answers stored in the local cache.
+	 * Gets an unmodifiable reference to the set of answers stored in the local
+	 * cache.
 	 * 
-	 * @return	Unmodifiable set containing local answers
+	 * @return Unmodifiable set containing local answers
 	 */
 	public Set<Answer> getAnswerSet() {
 		return Collections.unmodifiableSet(this.answerSet);
 	}
-	
+
 	/**
 	 * Creates a new Answer before inserting it into the Answers table and local
 	 * cache.
